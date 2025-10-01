f5cc7e0bd6ba80f0f1f669af243e6d1e6bdf45f1
fazlerabbi13 <mohammad0132794@gmail.com>
2025-10-01 14:31:27 +0600

    create push function for add element in stack.

--- Stack/LLimplementation.java
+++ Stack/LLimplementation.java
@@ -7,3 +7,24 @@ class Node {
         next = null;
     }
 }
+
+class myStack {
+    Node top;
+
+    int count;
+
+    myStack() {
+        top = null;
+        count = 0;
+    }
+
+    // push operation
+    void push(int x){
+        Node temp = new Node(x);
+        temp.next = top;
+        top = temp;
+
+        count++;
+    }
+    
+}
